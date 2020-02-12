782f8025406b26d4d76f84407a25a6f1d19376ae
AjHipolito <hipolito@ajency.in>
2020-02-12 12:57:37 +0530

    col fix

--- src/shared-components/expense-item.js
+++ src/shared-components/expense-item.js
@@ -14,30 +14,30 @@ class ExpenseItem extends Component {
 					<h3>AI</h3>
 					{/* <img src=> */}
 				</div>
-				<div className="item-col">
+				<div className="item-col col1">
 					<h3 className="title">{this.props.data.STRATEGY}</h3>
 					<h4 className="subTitle">Monthly AI Forcast</h4>
 					<span className="badge badge-grey"><span className="legends legends-orange"></span>{this.props.data.STRATEGY_TYPE}</span>					
 					<span className="badge badge-grey">Value</span>					
 				</div>
-				<div className="item-col">
+				<div className="item-col col2">
 					<h3>+ {this.props.data.LTM}<span>%</span></h3>
 				</div>
-				<div className="item-col">
+				<div className="item-col col3">
 					<h3>+ {this.props.data.ALPHA}<span>%</span></h3>
 				</div>
 
-				<div className="bechmark">
+				<div className="bechmark col4">
 					<h4>{this.props.data.BENCHMARK}</h4>
 					<label>Reussel 200 value ETF</label>
 				</div>
-				<div className="item-col">
+				<div className="item-col col5">
 				<Button title={'Details'} buttonClass={"buttonSecondary"} />
 				</div>
-				<div className="item-col">
+				<div className="item-col col6">
 					<h5>Fee: {this.props.data.FEE}%</h5>
 				</div>
-				<div className="item-col">
+				<div className="item-col col7">
 					<Button title={'Subscribe'} buttonClass={"buttonPrimary"} />
 					<label>{this.props.data.SUBSCRIBERS} Subscribers</label>
 				</div>
