6341c79196671e7ccf369be4ddd89f6f92168977
Viraj Verlekar <viraj@ajency.in>
2020-02-12 11:30:42 +0530

    basic components

---
+++ src/shared-components/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+	constructor(props)	{
+
+	}
+
+	rendor() {
+		return (
+			<></>
+		)
+	}
+}
+
+export default Header;
---
+++ src/shared-components/wrapper-header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class WapperHeader extends Component {
+	constructor(props)	{
+
+	}
+
+	rendor() {
+		return (
+			<></>
+		)
+	}
+}
+
+export default WapperHeader;
