091176629743145cd515ddded489d8f2436ee45b
AjHipolito <hipolito@ajency.in>
2020-02-12 12:46:57 +0530

    header

--- src/shared-components/wrapper-header.js
+++ src/shared-components/wrapper-header.js
@@ -8,16 +8,18 @@ class WapperHeader extends Component {
 	render() {
 		return (
 			<>
-				<div>Explore Strategies</div>
-				<div>Choose what you control and what decisions you want</div>
-				<div>Investment Themes</div>
+				<div className="header-wrapper">
+					<div>Explore Strategies</div>
+					<div>Choose what you control and what decisions you want</div>
+					<div>Investment Themes</div>
 
-				<div>
-					<div>All</div>
-					<div>Fundamental Factors</div>
-					<div>Alternative Data Factors</div>
-					<div>Quant Factors</div>
-					<div>Global Factors</div>
+					<div className="header">
+						<div className="headerItem">All</div>
+						<div className="headerItem">Fundamental Factors</div>
+						<div className="headerItem">Alternative Data Factors</div>
+						<div className="headerItem">Quant Factors</div>
+						<div className="headerItem">Global Factors</div>
+					</div>
 				</div>
 			</>
 		)
