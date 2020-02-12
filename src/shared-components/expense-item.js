4b966b928d2512fe988e5716d45265b095e355a8
AjHipolito <hipolito@ajency.in>
2020-02-12 12:33:58 +0530

    aaaa

--- src/shared-components/expense-item.js
+++ src/shared-components/expense-item.js
@@ -10,20 +10,20 @@ class ExpenseItem extends Component {
 	render() {
 		return (
 			<div className="cardWrapper">
-				<div>
+				<div className="item-col">
 					<label>All</label>
 					{/* <img src=> */}
 				</div>
-				<div>
+				<div className="item-col">
 					<h3 className="title">Long Only</h3>
 					<h4 className="subTitle">Monthly AI Forcast</h4>
 					<span className="badge badge-grey"><span className="legends legends-orange"></span> value</span>					
 					<span className="badge badge-grey">value</span>					
 				</div>
-				<div>
+				<div className="item-col">
 					<h3>+ 25.4<span>%</span></h3>
 				</div>
-				<div>
+				<div className="item-col">
 					<h3>+ 7.6<span>%</span></h3>
 				</div>
 
@@ -32,15 +32,15 @@ class ExpenseItem extends Component {
 					<label>Reussel 200 value ETF</label>
 				</div>
 
-				<div>
-					<Button>Details</Button>
+				<div className="item-col">
+					<Button className="">Details</Button>
 				</div>
-				<div>
+				<div className="item-col">
 					<h5>Fee: 10bps mgmnt</h5>
 					<label>50% Alpha Share</label>
 				</div>
-				<div>
-					<Button>Suscribe</Button>
+				<div className="item-col">
+					<Button className="buttonPrimary">Suscribe</Button>
 					<label>370 Subscribers</label>
 				</div>
 			</div>
